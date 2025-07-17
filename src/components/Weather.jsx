abe5a61729d52bc89d49fd6efdbc16ad16f364db
raidha12 <rfazal2001@gmail.com>
2025-07-17 15:27:45 +0530

    Added the search function

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -11,6 +11,7 @@ import humidity_icon from '../assets/humidity.png'
 
 const Weather = () => {
 
+    const inputRef = useRef()
     const [weatherData, setWeatherData] = useState(false);
 
     const allIcons = {
@@ -31,6 +32,10 @@ const Weather = () => {
     };
 
     const search = async (city) => {
+        if (city === "") {
+            alert("Enter City Name");
+            return;
+        }
         try {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
             const response = await fetch(url);
@@ -56,8 +61,8 @@ const Weather = () => {
     return (
         <div className='weather'>
             <div className='search-bar'>
-                <input type="text" placeholder='Search' />
-                <img src={search_icon} alt="" />
+                <input ref={inputRef} type="text" placeholder='Search' />
+                <img src={search_icon} alt="" onClick={() => search(inputRef.current.value)} />
             </div>
             <img src={weatherData.icon} alt="" className='weather-icon' />
             <p className='temperature'>{weatherData.temperature}°c</p>
